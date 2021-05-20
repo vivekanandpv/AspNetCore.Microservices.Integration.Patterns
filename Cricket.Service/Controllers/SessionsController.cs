704b00b8eb2f63ab725ab1027cb30eb23abeee31
vivek <vivek@athenaeum.in>
2021-05-20 07:41:57 +0530

    Add project files.

---
+++ Cricket.Service/Controllers/SessionsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cricket.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionsController : ControllerBase
+    {
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await Task.FromResult(new { Id = Guid.NewGuid().ToString(), Start = DateTime.Now.AddHours(-4), End = DateTime.Now, Player = "Amit", Facility = "WS30" }));
+        }
+    }
+}
---
+++ Player.Service/Controllers/PlayersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Player.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await Task.FromResult(new { Id = Guid.NewGuid().ToString(), Name = "Harish", City = "Mumbai" }));
+        }
+    }
+}
